c17f281a3fbff378498a046095f0330726e3b157
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-02-28 22:31:34 -0800

    Make a stupid Function

--- app/src/main/java/io/github/sanjit1/frcscouter/addTemplates.java
+++ app/src/main/java/io/github/sanjit1/frcscouter/addTemplates.java
@@ -437,7 +437,7 @@ public class addTemplates extends AppCompatActivity {
             writer.append("endActivity🤖🚀🤖🚀");
             EditText description = findViewById(R.id.description);
             writer.append(System.lineSeparator());
-            if(description.getText().toString()== null) description.setText(" ");
+            if(description.getText().toString()== null) description.setText("No description");
             writer.append(description.getText().toString());
             writer.flush();
             writer.close();
