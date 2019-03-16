eac202c8e46d54a4ca6c5c27a9cbe94b7119df4a
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-03-15 21:11:26 -0700

    yo i am hapy wit mai life

---
+++ app/src/main/java/io/github/sanjit1/frcscouter/Teams.java
@@ -0,0 +1,31 @@
+package io.github.sanjit1.frcscouter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import io.github.sanjit1.frcscouting.R;
+
+public class Teams extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
